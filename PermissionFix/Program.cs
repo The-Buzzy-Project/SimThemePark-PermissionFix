4e72451cc753f8b8defc8eaf396c1f64cc7c0adf
Duck Bread <ryanguy426@gmail.com>
2016-10-23 14:06:52 -0400

    Fully working program

--- PermissionFix/Program.cs
+++ PermissionFix/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Security.AccessControl;
+using System.Security.Principal;
 
 namespace PermissionFix
 {
@@ -10,7 +13,14 @@ namespace PermissionFix
     {
         static void Main(string[] args)
         {
-
+            changePermissions(Directory.GetCurrentDirectory());
+        }
+        static void changePermissions(string file)
+        {
+            DirectoryInfo dInfo = new DirectoryInfo(file);
+            DirectorySecurity dSecurity = dInfo.GetAccessControl();
+            dSecurity.AddAccessRule(new FileSystemAccessRule(new SecurityIdentifier(WellKnownSidType.WorldSid, null), FileSystemRights.FullControl, InheritanceFlags.ObjectInherit | InheritanceFlags.ContainerInherit, PropagationFlags.NoPropagateInherit, AccessControlType.Allow));
+            dInfo.SetAccessControl(dSecurity);
         }
     }
 }
